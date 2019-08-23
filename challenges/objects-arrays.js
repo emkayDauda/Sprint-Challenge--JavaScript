038c8ca571e44ac463e8d171c808f1879fa96dd1
Maaruf Dauda <mkdauda.work@gmail.com>
2019-08-23 08:47:58 +0100

    Filter and map graduates' universities containing the string 'Uni'

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -89,7 +89,8 @@ console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+var uni = [];
+uni = graduates.filter(graduate => graduate.university.includes(`Uni`)).map(graduate => graduate.university)
 console.log(uni);
 
 
